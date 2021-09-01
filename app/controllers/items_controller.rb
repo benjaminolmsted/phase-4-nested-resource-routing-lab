4633158c3910b6ca07ba20bf33aa963797522230
benjamin <benjamin.olmsted@gmail.com>
2021-09-01 06:54:24 -0700

    commit tree 3d9aa9ed7f2b8c87970a06e8de16f8cc398ae4a8

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,36 @@
 class ItemsController < ApplicationController
-
+rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_response
+  
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
   end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item, include: :user
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    item = user.items.create(item_params)
+    render json: item, status: :created
+  end
+
+  private 
+
+  def record_not_found_response
+    render json: {error: "user not found"}, status: :not_found
+  end
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+
 
 end
